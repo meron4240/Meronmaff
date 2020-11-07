e88be77c91b9058a82f252815c3aefe842c35a1a
meron4240 <73767630+meron4240@users.noreply.github.com>
2020-11-07 05:48:07 +0300

    book

--- src/Maff_1/mulesoftbookpracties.java
+++ src/Maff_1/mulesoftbookpracties.java
@@ -3,7 +3,5 @@ package Maff_1;
 public class mulesoftbookpracties {
     public static void main(String[] args) {
         System.out.println("a book about maff &ft");
-        System.out.println("today is there anniversary");
-        System.out.println("they are celebrating their six year anniversary");
     }
 }
